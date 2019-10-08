ec170ebb3eec4d418f91f075945e97583d87b884
ejversoza <40896958+ejversoza@users.noreply.github.com>
2019-10-07 17:51:38 -0700

    Add files via upload

---
+++ CS-530-Program1/xbd.h
@@ -0,0 +1,20 @@
+/**************************************************************************************
+Name:Emily Jean Versoza(819442111) and Christina Tran(819312540)  
+Username: cssc0422, cssc0466
+Project: Assignment1
+File: xbd.h
+Note: header file for xbd.c
+***************************************************************************************/
+
+	#include<stdio.h> 
+	#include<string.h>
+
+	#define BIN 6 //global varible for binary 
+	#define HEX 16 // global variable for hex
+	
+	
+
+	int pBin(); //function prototype for pBin
+	int pHex(); //fucntion prototype for pHex
+	
+
